ed6a54cc846b4ef16c2fdbb4dc2e5f8069c93f32
gigaxucishvili2007 <158300399+gigaxucishvili2007@users.noreply.github.com>
2024-10-11 13:20:07 +0400

    Add files via upload

---
+++ tech english project/third lesson/homework/lesson3.py
@@ -0,0 +1,64 @@
+# # homework:
+
+# choose - არჩევა
+
+# following - შემდეგ
+
+# different - განსხვავებული
+
+# opportunities - შესაძლებლობები
+
+# worth - ღირს
+
+# effort - ძალისხმევა
+
+# reduces - ამცირებს
+
+# enhances - აძლიერებს
+
+# things - რამ/რაღაცეები
+
+# expand - გაფართოება
+
+# imagination - ფანტაზია/მოგონება
+
+# couldn't - ვერ შეძლო
+
+# arrive - მოსვლა
+
+# TRANSLATE THIS-
+
+# Health is one of the most important aspects of life. When a person is healthy, they can participate fully in all the activities that life offers. Whether it’s working, spending time with family, or pursuing hobbies, good health allows us to enjoy life to the fullest. Without it, many of the things we love become difficult or impossible.
+
+# სიცოცხლე არის ყველაზე იმპორტირებული ასპექტი ცხოვრებაში როდესაც ადამიანი არის ჯანსაღი შეუძლია ყველაფერი აქტივობა რომ ცხოვრებაში იმუშავოს გამოიმუშავოს დრო ცხოვრებაში რომ გარეშე რაღაცეები ჩვენ სიყვარული რთული ან
+# (სწორად გადავთარგმნე თუ არა არ ვიცი ვისწავლი უფრო დროთა განმავლობაში)
+# they
+# participate
+# fully
+# offers
+# Whether
+# fullest
+# it
+# many
+# impossible
+
+
+
+# When someone is healthy, they have the energy and strength to take part in many activities. Being active not only keeps the body strong but also improves mental well-being. Activities like sports, walking, or even simple everyday tasks can help people stay fit. Participating in these activities allows us to stay connected with the world around us, leading to a more fulfilled life.
+
+# როდესაც არის ჯანსაღი გვაქვს ენერგია და ძალა რომ მივიღოთ ნაწილი აქტივობების აქტიური არა მხოლოდ განაგრძო შენი სხეულის სიძლიერე მაგრამ გააუმჯობესე მენტალიტეტი  აქტიურობის მოგვწონს სპორტი ან ლუწი მარტივი ყოველდღიურობის დავალებები შეგიძლია შენ დაეხმარო ხალხს ნებადართული დაუკავშირე სამყაროსთან რომ მეტი სავსე ცხოვრება გქონდეს (სწორად გადავთარგმნე თუ არა არ ვიცი)
+# someone
+# they
+# many
+# Being
+# well
+# being
+# walking
+# stay
+# fit
+# Participating
+# these
+# around
+# us
+# leading
+# fulfilled
