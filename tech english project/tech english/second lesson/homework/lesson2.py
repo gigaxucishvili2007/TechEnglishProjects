1af4306718150c384879d5f3c5eefcedb69fe0d7
gigaxucishvili2007 <158300399+gigaxucishvili2007@users.noreply.github.com>
2024-10-11 13:26:23 +0400

    Add files via upload

---
+++ tech english project/tech english/second lesson/homework/lesson2.py
@@ -0,0 +1,59 @@
+# # execute - მუშაობს
+
+# properly - სწორად
+
+# figure out - გაარკვიო
+
+# turn off - გამორთვა
+
+# notification - მესიჯი
+
+# bonus - ბონუსი
+
+# rubbing - ხახუნი
+
+# anticipation - მოლოდინი
+
+# going to make - გააკეთებს
+
+# multiplied - გამრავლდეს
+
+# salary - ხელფასი
+
+# called - სახელად, კიდევ შეიძლება იყოს დარეკა
+
+# usually - ჩვეულებრივ
+
+# create 3 examples of while loops
+
+# 15-fifteen
+
+# 16-sisxteen
+
+# 17-seventeen
+
+# 18-eighteen
+
+# 19-ninteen
+
+# 20-twenty
+
+# 21-twenty one
+
+# 22-twenty two
+
+# 23-twenty three
+
+# 24-twenty four
+
+# 25-twenty five
+
+# 26-twenty six
+
+# 27-twenty seven
+
+# 28-twenty aight
+
+# 29-twenty nine
+
+# 30-thirty
